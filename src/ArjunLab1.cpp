50329d12915757f6f97e341141022b0b6bf0e41c
generalrelativity9371 <max_beyer@dalton.org>
2023-06-26 11:51:13 -0500

    test

---
+++ src/ArjunLab1.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/student/ArjunLab1/src/ArjunLab1.ino"
+/*
+ * Project ArjunLab1
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/student/ArjunLab1/src/ArjunLab1.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
